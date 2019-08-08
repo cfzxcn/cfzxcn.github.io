6c36d1a52080a2b3dad11bc0bc44fbd073b2f126
Andy <604174410@qq.com>
2019-08-08 14:06:30 +0800

    首页基本做出了效果在banner.html，但还有很多小问题在

--- js/index.js
+++ js/index.js
@@ -1,20 +1,17 @@
 $(function () {
-     // 1. 轮播图
-    $(window).on('resize',  ()=> {
-        // 1.获取窗口的宽度
+    // 1. 轮播图
+    $(window).on('resize', () => {
+        // 1.窗口的宽度
         let clientW = $(window).width();
         // 2. 设置临界点
-        let isShowBigImage = clientW >= 500;
+        let isShowBigImage = clientW >= 600;
         // 3. 获取所有item
-        let $allItems = $('#cf_carousel1 .carousel-item');
-        // console.log($allItems); //测试是否拿到所有的carousel-item对象
-        
+        let $allItems = $('#cf_carousel .carousel-item');
+        // console.log($allItems);
         // 4. 遍历
         $allItems.each((index, item) => {
-            //console.log(Item);//测试是否拿到切图图的div
             // 4.1 取出图片的路径
             let src = isShowBigImage ? $(item).data('lg-img') : $(item).data('sm-img');
-            //console.log(src); 测试不同浏览器宽度下是否取得图片地址
             let imgUrl = `url(${src})`;
             // 4.2 设置背景
             $(item).css({
@@ -32,8 +29,7 @@ $(function () {
     $(window).trigger('resize');
 
     // 3. 添加轮播图的滑动
-    let startX = 0,
-        endX = 0;
+    let startX = 0, endX = 0;
     let carouselInner = $('#cf_carousel .carousel-inner')[0];
     let $carousel = $('#cf_carousel');
     let carousel = $carousel[0];
@@ -43,41 +39,70 @@ $(function () {
     });
     carouselInner.addEventListener('touchmove', (e) => {
         endX = e.targetTouches[0].clientX;
-        if (endX - startX > 0) { // 上一张
+        if (endX - startX > 0) {  // 上一张
             $carousel.carousel('prev');
         } else if (endX - startX < 0) { // 下一张
             $carousel.carousel('next');
         }
     });
 
-
-
-    // 4. 超出内容处理
-    $(window).on('resize', () => {
+    // 4. 超出内容处理，这块负责公司产品在小屏下出现横向滚动条，配合html中的2条新增style使用；
+    $(window).on('resize', ()=>{
         let $ul = $('#cf_product .nav');
         let $allLis = $('.nav-item', $ul);
         let totalW = 0; // 所有li的宽度
-        $allLis.each((index, item) => {
+        $allLis.each((index, item)=>{//这句里的item是JS对象
             totalW += $(item).width();
         });
         // console.log(totalW);
 
         // 获取父标签的宽度
         let parentW = $ul.parent().width();
-        // console.log(parentW);
-        if (totalW > parentW) {
+         //console.log(parentW);
+        if(totalW > parentW) {
             $ul.css({
                 width: totalW + 'px'
             })
-        } else {
+        }else {
             $ul.removeAttr('style');
         }
     }).trigger('resize');
 
-// 2. 工具的提示 ,放到上面不起作用
-$('[data-toggle="tooltip"]').tooltip();
+    // 2. 工具的提示
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
+
+
+
+
+//图集切换特效开始
+$(".filter li a").click(function () {
+    var filterValue = $(this).attr('data-filter');
+    $(".isotope-container").isotope({
+        filter: filterValue
+    });
+
+    $(this).addClass('active').parent().siblings().children().removeClass('active');
+    return false;
+});
+//图集切换特效结束
+
+//滚动监听
+$('body').scrollspy({
+    target: '#cfnavbar'
+})
+
+//平滑滚动 ,这里可控制折叠菜单的二级菜单，取消此JS二级菜单在折叠时无法弹出
+$(".navbar a").click(function (event) {
+//    console.log(this.hash);
+    var target = $(this.hash);
+    //    console.log(target.offset().top);
+      $("html").animate({
+        scrollTop:target.offset().top-150
+      },500);
+
+});
 
 
 
@@ -102,7 +127,8 @@ headerInit();
 
 
 
-//公司产品样式开始（杀毒软件操作系统ORACLE网页制作木兰词画堂春浣溪沙采桑子一剪梅）使用淡入效果，内容延迟出现的特效  ====================================================
+
+//公司产品样式开始（杀毒软件操作系统ORACLE网页制作木兰词画堂春浣溪沙采桑子一剪梅）使用淡入效果，内容延迟出现的特效，使用淡入效果，内容延迟出现的特效  开始====================================================
 $(function () {
 if ($("[data-animation-effect]").length > 0) {
     $("[data-animation-effect]").each(function (index, el) {
@@ -114,56 +140,19 @@ if ($("[data-animation-effect]").length > 0) {
         }, 800);
     });
 }
-       });
+    });
 //使用淡入效果，内容延迟出现的特效  结束====================================================
 
-
-
-
-//图集切换特效开始
-$(".filter li a").click(function () {
-    var filterValue = $(this).attr('data-filter');
-    $(".isotope-container").isotope({
-        filter: filterValue
+/*向上功能开始*/
+$(function() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 400)
+            $('div.go-top').show();
+        else
+            $('div.go-top').hide();
     });
-
-    $(this).addClass('active').parent().siblings().children().removeClass('active');
-    return false;
+    $('div.go-top').click(function() {
+        $('html, body').animate({scrollTop: 0}, 1000);
+    });
 });
-
-//图集切换特效结束
-
-//滚动监听
-$('body').scrollspy({
-    target: '#cfnavbar'
-})
-
-//平滑滚动
-$(".navbar a").click(function (event) {
-//    console.log(this.hash);
-    var target = $(this.hash);
-    //    console.log(target.offset().top);
-      $("html").animate({
-        scrollTop:target.offset().top-150
-      },500);
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*向上功能结束*/
